6ad2660b5f8645a3f17cc7ba4c8c5e2ac7ef4810
swapno5896 <swapno5896@yahoo.com>
2021-08-12 18:05:15 +0600

    os id added

---
+++ os.js
@@ -0,0 +1,4 @@
+const os = require('os');
+// console.log(os.platform()) // tells operating sestem name
+// console.log(os.homedir()) // tells home dir name 
+// console.log(os.freemem()) // tells how much free memory we have
