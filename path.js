836b973636af20b3169792462bd0d8dd40840bb5
swapno5896 <swapno5896@yahoo.com>
2021-08-12 18:00:45 +0600

    path is done

---
+++ path.js
@@ -0,0 +1,5 @@
+const path = require('path');
+const myPath = 'C:/Works/Backend/Core-Module/path.js';
+// console.log(path.dirname(myPath)) // tells current dir name
+//  console.log(path.basename(myPath)) // tells current basename
+//  console.log(path.extname(myPath)) // tells extentaion of file 
